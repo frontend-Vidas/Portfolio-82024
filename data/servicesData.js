7c7a1d8353452fcb1849bdd8a594a39728cb5acb
tropikais <desertukas@yahoo.com>
2024-09-05 11:48:53 +0300

    update

---
+++ data/servicesData.js
@@ -0,0 +1,34 @@
+const servicesData = [
+    {
+        icon: 'desktop',
+        title: 'Search optimization',
+        desc: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    {
+        icon: 'layers',
+        title: 'Logo &amp; identity',
+        desc: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    {
+        icon: 'hotairballoon',
+        title: 'Graphics design',
+        desc: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    {
+        icon: 'mobile',
+        title: 'Fully responsive',
+        desc: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    {
+        icon: 'tools-2',
+        title: 'Advanced options',
+        desc: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+    {
+        icon: 'scissors',
+        title: 'Reasonable pricing',
+        desc: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
+    },
+];
+
+export { servicesData };
---
+++ js/components/socialMetrics.js
@@ -0,0 +1,69 @@
+// return -> error-first
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    const x = window.innerWidth || document.documentElement.clientWidth;
+    const y = window.innerHeight || document.documentElement.clientHeight;
+
+    return rect.bottom > 0 && rect.right > 0 && rect.left < x && rect.top < y;
+}
+
+function socialMetrics(selector, data) {
+    if (typeof selector !== 'string' || selector === '') {
+        return [true, 'ERROR: selector has to be non-empty string'];
+    }
+
+    const DOM = document.querySelector(selector);
+    if (DOM === null) {
+        return [true, 'ERROR: could not find DOM element'];
+    }
+
+    if (!Array.isArray(data) || data.length === 0) {
+        return [true, 'ERROR: data has to be non-empty array'];
+    }
+
+    // Generuojame turini
+    let HTML = '';
+
+    for (const item of data) {
+        HTML += `<li class="metric">
+                    <div class="number">0${item.symbol}</div>
+                    <div class="label">${item.label}</div>
+                </li>`;
+    }
+
+    DOM.classList.add('social-metrics');
+    DOM.innerHTML = HTML;
+
+    // Stebim scroll'a
+    const metricsDOM = DOM.querySelectorAll('.metric');
+    const animatedMetrics = new Array(metricsDOM.length).fill(false);
+
+    window.addEventListener('scroll', () => {
+        const totalLoadingTimeMs = 3000;
+        for (let i = 0; i < metricsDOM.length; i++) {
+            const metricDOM = metricsDOM[i];
+
+            if (animatedMetrics[i]) {
+                continue;
+            }
+
+            if (isElementInViewport(metricDOM)) {
+                animatedMetrics[i] = true;
+
+                let count = 0;
+                const timer = setInterval(() => {
+                    metricDOM.querySelector('.number').textContent =
+                        ++count + data[i].symbol;
+                    if (count >= data[i].number) {
+                        clearInterval(timer);
+                    }
+                }, totalLoadingTimeMs / data[i].number);
+            }
+        }
+    });
+
+    return [false, 'OK'];
+}
+
+export { socialMetrics };
